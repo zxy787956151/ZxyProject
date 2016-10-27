c0e6f27a55d7520584c0375b2c45b9331adabe30
YiZhi <1780606427.com>
2016-10-27 20:48:15 +0800

    com 10-27 20:48

--- www/myphp/E-commerce/Application/Home/Controller/ProductController.class.php
+++ www/myphp/E-commerce/Application/Home/Controller/ProductController.class.php
@@ -6,35 +6,36 @@
 	Class ProductController extends Controller{
 
 		Public function index($pd){
-			var_dump($pd);
-			die();
-			// $this->arr = D('category')->getTree(M('category')->select(),0);
+			// var_dump($pd);
+			// die();
+			
+			$this->arr = D('category')->getTree(M('category')->select(),0);
 
-			// $Dpro = D('Product');
+			$Dpro = D('Product');
 
-			// $GLOBALS['pagNum'] = $Dpro->GetPageNum('Product','8');
+			$GLOBALS['pagNum'] = $Dpro->GetPageNum('Product','8');
 
-			// //ChangePage
-			// switch ($_POST['pd']) {
-			// 	case '':
-			// 		$_SESSION['NowPage'] = '1';
-			// 		$this->PageNow = $Dpro->group('Product','9','1',$GLOBALS['pagNum']);
-			// 		break;
+			//ChangePage
+			switch ($_POST['pd']) {
+				case '':
+					$_SESSION['NowPage'] = '1';
+					$this->PageNow = $Dpro->group('Product','9','1',$GLOBALS['pagNum']);
+					break;
 
-			// 	case '0':
-			// 		// $this->PageNow = $Dpro->group('Product','9','1',$GLOBALS['pagNum']);
-			// 		break;
+				case '0':
+					// $this->PageNow = $Dpro->group('Product','9','1',$GLOBALS['pagNum']);
+					break;
 
-			// 	case '1':
-			// 		$_SESSION['NowPage']+=1;
-			// 		$this->PageNow = $Dpro->group('Product','9',$_SESSION['NowPage'],$GLOBALS['pagNum']);
-			// 		break;
+				case '1':
+					$_SESSION['NowPage']+=1;
+					$this->PageNow = $Dpro->group('Product','9',$_SESSION['NowPage'],$GLOBALS['pagNum']);
+					break;
 
-			// 	default:
-			// 		break;
-			// }
+				default:
+					break;
+			}
 
-			// var_dump($GLOBALS['pagNum']);
+			var_dump($GLOBALS['pagNum']);
 
 
 			$this->display();
